bb8a9338f6dcaa52644f253707e1746b5ba37607
James Nguyen <ndhoang7089@gmail.com>
2024-05-20 17:40:51 +0700

    merge Sprint2 to course repo

---
+++ src/main/java/testDummyAPI/sprint2/Sprint2Application.java
@@ -0,0 +1,15 @@
+package testDummyAPI.sprint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint2Application.class, args);
+	}
+
+
+
+}
